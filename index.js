18254e834a75ac53d61f9ebce75a92b3a39afa8d
naimurrahmanniloy <niloyrahman432@gmail.com>
2022-11-10 14:28:37 +0600

    done with the full structure

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 const express = require('express')
 const cors = require('cors');
 const app = express();
+const port = process.env.PORT || 5000;
+
+//middle wares
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send('Running the server')
+})
+
+app.listen(post, () => {
+    console.log(`Gym server on ${port}`)
+})
